ce5beb4799e7842f19b819069ea7c207096118db
Jared A Sutton <jaredasutton@gmail.com>
2017-10-27 14:48:27 -0400

    Create populateAllStudents function.

---
+++ utilities/populateAllStudents.js
@@ -0,0 +1,16 @@
+var connection = require('../hive-sql.js')
+var fs = require('fs')
+
+var query = "select concat(firstName,' ',lastName) as name, entryID, classNo from hive1718.userDirectory where courseStr regexp 's' order by classNo, lastName"
+
+connection.query(query,function(err,res,fds){
+	if(err) throw err
+	fs.writeFile('../devutil/allstudents.js',
+		JSON.res.map((item) => ({name:item.name,entryID:item.entryID,classNo:item.classNo})),
+		(errw)=> {
+			if(errw) throw errw
+			console.log('the file saved')
+		}
+	)
+})
+
