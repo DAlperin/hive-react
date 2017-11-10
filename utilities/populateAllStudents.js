fbaf90c079fe1673253902ed30d1414cd113effc
Jared A Sutton <jaredasutton@gmail.com>
2017-11-10 14:51:19 -0500

    Fix populator. (json.parse)

--- utilities/populateAllStudents.js
+++ utilities/populateAllStudents.js
@@ -1,16 +1,27 @@
 var connection = require('../hive-sql.js')
 var fs = require('fs')
 
-var query = "select concat(firstName,' ',lastName) as name, entryID, classNo from hive1718.userDirectory where courseStr regexp 's' order by classNo, lastName"
+var query = "select concat(firstName,' ',lastName) as name, " +
+	"entryID, " +
+	"classNo " +
+	"from hive1718.userDirectory " +
+	"where courseStr regexp 's' " +
+	"order by classNo, lastName"
 
-connection.query(query,function(err,res,fds){
-	if(err) throw err
-	fs.writeFile('../devutil/allstudents.js',
-		JSON.res.map((item) => ({name:item.name,entryID:item.entryID,classNo:item.classNo})),
-		(errw)=> {
-			if(errw) throw errw
-			console.log('the file saved')
-		}
-	)
-})
+connection.query(query,
+	function(err,res,fds){
+		if(err) throw err
+		fs.writeFile('../devutil/allstudents.js',
+			JSON.parse(res).map(
+				(item) => (
+					{name:item.name,entryID:item.entryID,classNo:item.classNo}
+				)
+			),
+			(errw)=> {
+				if(errw) throw errw
+				console.log('the file saved')
+			}
+		)
+	}
+)
 
