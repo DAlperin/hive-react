615bb93086e7d0845ff652625b7d98808fdefb1f
Jared A Sutton <jaredasutton@gmail.com>
2017-11-10 13:59:10 -0500

    Untrack rosters in devutil.

--- components/Mastery/utilities/parseAssessment.js
+++ components/Mastery/utilities/parseAssessment.js
@@ -10,26 +10,69 @@ function parseAssessment(mArr){
     var mRatingStrs={}
     var stuBios={}
     var LOsByID = {}
-    LOs.forEach((LO)=>{LOsByID[LO.LOID] = LO})
+    LOs.forEach(
+        (LO)=>{
+            LOsByID[LO.LOID] = LO
+        }
+    )
     console.log("LOs")
     console.log(LOsByID)
     var alignModel = {}
-    assessmentModel.LOAlign.split("n").forEach((str)=>{if(!!str){ alignModel[str.substring(1)] = {}}})
+    assessmentModel.LOAlign
+        .split("n")
+          .forEach(
+            (str)=>{
+              if(!!str){
+                alignModel[str.substring(1)] = {}
+              }
+            }
+          )
     
     var columnHeads = Object.assign({},alignModel)
     var AssessmentArrS = Object.assign({},alignModel)
-    Object.keys(AssessmentArrS).forEach((LOID,id)=>{
-        AssessmentArrS[LOID] = {mcountN:0,mcountA:0,mcountM:0,mcountE:0,mcountU:0, LOText:LOsByID[LOID].LOText}
-    })
+    Object.keys(AssessmentArrS).forEach(
+      (LOID,id)=>{
+        AssessmentArrS[LOID] = {
+          mcountN:0,
+          mcountA:0,
+          mcountM:0,
+          mcountE:0,
+          mcountU:0, 
+          LOText:LOsByID[LOID].LOText
+        }
+      }
+    )
     var rowsByStu = {}
     
-    studentRows.forEach((row,id) => {
-        if((!!classNoF && row.classNo.indexOf(classNoF) < 0) || (!!studentName && (row.title + ' ' + row.lastName).toLowerCase().indexOf(studentName.toLowerCase()) < 0 ) ) return
+    studentRows.forEach(
+      (row,id) => {
+        if(
+          (
+            !!classNoF && row.classNo.indexOf(classNoF) < 0
+          ) 
+          ||
+          (
+            !!studentName
+            &&
+            (row.title + ' ' + row.lastName).toLowerCase()
+              .indexOf(studentName.toLowerCase()) < 0
+          )
+          ||
+          (
+            false
+          )
+          ) {return}
         stuBios[row.uEntryID] = row.title + " " + row.lastName
         mRating0s[row.uEntryID] = {0:0,1:0,2:0,3:0,4:0}
         mRatingStrs[row.uEntryID] = ""
-        if(!rowsByStu.hasOwnProperty(row.uEntryID)){rowsByStu[row.uEntryID] = {ratings:Object.assign({},alignModel),studentRowsId:id,recentrating:''}
-        Object.keys(rowsByStu[row.uEntryID].ratings).forEach((key,id)=>{
+        if(!rowsByStu.hasOwnProperty(row.uEntryID)){
+          rowsByStu[row.uEntryID] = {
+            ratings:Object.assign({},alignModel),
+            studentRowsId:id,
+            recentrating:''
+          }
+        Object.keys(rowsByStu[row.uEntryID].ratings).forEach(
+          (key,id)=>{
             var ratingsREGEXP = new RegExp('m' + key + ':[0-4]n')
             if(ratingsREGEXP.exec(row.recentrating)){
                 var regmatch = ratingsREGEXP.exec(row.recentrating)[0]
@@ -74,6 +117,13 @@ function parseAssessment(mArr){
         }
     })
     console.log({rowsByStu:rowsByStu,alignModel:alignModel,AssessmentArrS:AssessmentArrS})
-    return {rowsByStu:rowsByStu,alignModel:alignModel,AssessmentArrS:AssessmentArrS,mRating0s:mRating0s,mRatingStrs:mRatingStrs,stuBios:stuBios}
+    return {
+      rowsByStu:rowsByStu,
+      alignModel:alignModel,
+      AssessmentArrS:AssessmentArrS,
+      mRating0s:mRating0s,
+      mRatingStrs:mRatingStrs,
+      stuBios:stuBios
+    }
 }
 module.exports = parseAssessment
