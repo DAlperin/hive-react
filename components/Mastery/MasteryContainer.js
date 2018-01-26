797e07777c219a28707897041ed33ce4b3d11da3
Jared A Sutton <jaredasutton@gmail.com>
2018-01-25 23:18:58 -0500

    Implement lo-stu-rting response.

--- components/Mastery/MasteryContainer.js
+++ components/Mastery/MasteryContainer.js
@@ -153,7 +153,9 @@ class MasteryContainer extends React.Component {
     
     return(
       <div className="container">
-        <div id="gradepopover" hidden={this.state.loAssessHidden}>{JSON.stringify(this.state.loAssessArr)}</div>
+        <div id="gradepopover" hidden={this.state.loAssessHidden}>
+          {this.state.loAssessArr.map((item)=>(<p><strong>item.AssessTitle</strong> {item.AssessDate.substring(5,10) + '-' + item.AssessDate.substring(0,5)}</p>))}
+        </div>
         {/*<ul className="pager">
           <li className={"previous" + ((this.state.page == 0) ? " disabled" : "") } onClick={this.prevPage}><a href="#" onClick={function(event){event.preventDefault();}}>Previous</a></li>
           <li className="next" onClick={this.nextPage}><a href="#" onClick={function(event){event.preventDefault();}}>Next</a></li>
